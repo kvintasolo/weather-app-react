678d2f9bbae7566d976094236061bd4a461faf20
Anna Stratanovych <105247537+kvintasolo@users.noreply.github.com>
2022-11-15 23:47:42 +0200

    Added API

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(){
+const [ready, setReady]=useState(false);
+const [weatherData,setweatherData] =useState({});
+function handleResponse(response){
+  setweatherData({
+    temperature:response.data.temperature.current,
+    city: response.data.city,
+    wind:response.data.wind.speed,
+    realfeel:response.data.temperature.feels_like,
+
+  });
+
+  setReady(true);
+}
+if (ready){
   return(
     <div className="Weather">
       <div className="searchBar">
@@ -18,11 +33,11 @@ export default function Weather(){
         </div>
        <div className="situation">
       <h1>I'TS CLOUDY IN</h1>
-      <h1> KYIV</h1>
+      <h1>{weatherData.city}</h1>
       </div>
       <div className="row">
         <div className="col-4">
-<h2> 12<div className="float">°C |°F</div></h2>
+<h2> {Math.round(weatherData.temperature)}<div className="float">°C |°F</div></h2>
 <h3>MAX 13°
 <br />
 MIN 10°
@@ -36,7 +51,7 @@ MIN 10°
         <div className="col-7">
           <h4>
             <ul>
-              <li>Kyiv</li>
+              <li>{weatherData.city}</li>
               <li>Wednesday 9 Nov 2022</li>
               <li>15:27</li>
             </ul>
@@ -46,7 +61,7 @@ MIN 10°
           <h4>
             Real feel 12°
             <br />
-            Wind 2 km/h1
+            Wind {weatherData.wind} km/h
             <br />
             Humidity 80%
 
@@ -56,5 +71,16 @@ MIN 10°
 
     </div>
     
-  )
+  );
+  
+}
+else{
+
+  const apiKey="6a3c54ed0f54ba33o91e65bbf9bt60ae";
+  let city="Kyiv"
+  let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
+axios.get(apiUrl).then(handleResponse); 
+return "Loading..."
+}
+
 }
