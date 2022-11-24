c895328a70f2e72598157196a5275d355c8a948a
Anna Stratanovych <105247537+kvintasolo@users.noreply.github.com>
2022-11-24 02:10:31 +0200

    forecast one day

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,23 +1,35 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, {useState} from "react";
+
+import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
 export default function WeatherForecast(props){
+    let [loaded, setLoaded]= useState(false);
+    let [forecast, setForecast]= useState(null);
     function handleResponse(response){
         console.log(response.data);
+        setForecast(response.data.daily);
+        setLoaded(true);
     }
-    let apiKey= "6a3c54ed0f54ba33o91e65bbf9bt60ae"
+    if (loaded){
+        return (
+        <div className="WeatherForecast"><div className="row">
+        <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+            
+            </div></div></div>);
+    
+    
+}
+else{
+    
+  
+
+
+let apiKey= "6a3c54ed0f54ba33o91e65bbf9bt60ae"
     let longitude=props.coordinates.longitude;
     let latitude=props.coordinates.latitude;
     let apiUrl=`https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`
     axios.get(apiUrl).then(handleResponse);
-    
-    
-    return(
-    <div className="WeatherForecast"><div className="row">
-        <div className="col">
-            <div className="WeatherForecast-day">Thu</div>
-            <WeatherIcon code="mist-night" size={36} />
-            <div className="WeatherForecast-temperature">19</div>
-            </div></div></div>);
-}
+    return null;
+}}
